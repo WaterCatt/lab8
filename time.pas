30bf609d25e699dcac946ab2afeed6c5a74c5a26
Илья Череповский <WaterCatt@yandex.ru>
2023-09-30 10:44:55 +0300

    Добавил программу определения високосности года

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  Assert(year > 0);
+  if (year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0)) then
+    Print('Год високосный!')
+  else Print('Год не високосный!');
+end.
